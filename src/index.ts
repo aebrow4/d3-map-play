2c16f4e23c2c109d09ff25abb6b8741f9fb39073
Andy Brown <andrew.ezra.brown@gmail.com>
2023-09-21 09:11:14 -0700

    Add d3-zoom and basic zoom behavior

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,29 @@ import { plotHartford } from './hartford'
 const width = 975
 const height = 610
 const svg = d3.select('body').append('svg').attr('viewBox', `0, 0 ${width}, ${height}`)
-svg.append(() => mapGroup.node())
-svg.append(() => plotHartford())
+
+/**
+ * Structure of our SVG:
+ * <svg>
+ *   <g> (containerGroup)
+ *     <g> (mapGroup)
+ *       <path /> (nation path)
+ *       <path /> (states path)
+ *       <path /> (counties path)
+ *     </g>
+ *     <g> (overlayGroup)
+ *       <circle /> (the overlays)
+ *       ...
+ *     </g
+ *   </g>
+ * </svg>
+ */
+const containerGroup = svg.append('svg:g')
+containerGroup.append(() => mapGroup.node())
+containerGroup.append(() => plotHartford())
+
+function zoomed ({ transform }: { transform: () => any }): void {
+  containerGroup.attr('transform', transform)
+}
+const zoom = d3.zoom<SVGSVGElement, unknown>()
+svg.call(zoom.on('zoom', zoomed))
